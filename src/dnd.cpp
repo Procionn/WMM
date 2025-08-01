0fba994c207c31b94d8194c59f76183d289b97be
Procion <procion362@gmail.com>
2025-08-01 18:50:41 +0300

    dnd module fix

--- src/dnd.cpp
+++ src/dnd.cpp
@@ -16,14 +16,11 @@
  */
 #include "dnd.h"
 
-#include "CONSTANTS.h"
 #include "core.h"
 #include "patterns/ERRORdialog.h"
 #include "methods.h"
 #include "ModManager.h"
 
-#include <iostream>
-#include <hpp-archive.h>
 #include <QDragEnterEvent>
 #include <QMimeData>
 
@@ -49,39 +46,12 @@ void CDND::dragEnterEvent(QDragEnterEvent* e) {
 }
 
 void CDND::dropEvent(QDropEvent* e) {
-    if (CConfigs::CONFIG_GAME != "None") {
+    if (Core::CONFIG_GAME != "None") {
         try {
             for (const QUrl &url : e->mimeData()->urls()) {
                 std::string fileName = url.toLocalFile().toStdString();
                 stc::cerr("Dropped file:" + fileName);
-#if 0
-                regex(fileName);
-                std::filesystem::path modsDir = stc::cwmm::ram_mods(name);
-                if (std::filesystem::exists(modsDir))
-                    stc::fs::remove_all(modsDir);
-                modInfoSave(modsDir);
-
-                ArchiveReader archive(fileName);
-                archive.set_export_directory(modsDir);
-                for (const auto* entry : archive)
-                    archive.write_on_disk();
-
-
-                std::filesystem::path path = ARCHIVE + Core::CONFIG_GAME + "/" + id;
-                std::filesystem::create_directories(path);
-                std::ofstream modStructFile(path / (version + EXPANSION2));
-                for (const auto& entry : std::filesystem::recursive_directory_iterator(modsDir)) {
-                    if (std::filesystem::is_regular_file(entry.path())) {
-                        std::filesystem::path relative = std::filesystem::relative(entry.path(), modsDir);
-                        modStructFile << relative.generic_string() << "\n";
-                    }
-                }
-                modStructFile.close();
-
-                stc::cerr("The archive has been successfully extracted to the folder: " + modsDir.string());
-#else
                 ModManager::get().load(fileName);
-#endif
             }
             emit launch();
         }
@@ -90,18 +60,4 @@ void CDND::dropEvent(QDropEvent* e) {
         }
     }
     else ERRORdialog* dialog = new ERRORdialog(Core::lang["LANG_LABEL_R37"]);
-}
-
-
-void CDND::modInfoSave(const std::filesystem::path& directory) {
-#if 0
-    std::filesystem::path path = directory / (WML + name + EXPANSION2);
-    std::filesystem::create_directories(path.parent_path());
-
-    std::ofstream file(path);
-    file << name << "\n";
-    file << version << "\n";
-    file << id << "\n";
-    file.close();
-#endif
 }
--- src/dnd.h
+++ src/dnd.h
@@ -19,16 +19,10 @@
 
 #include <QVBoxLayout>
 #include <QLabel>
-#include <filesystem>
 
 class CDND : public QLabel
 {
     Q_OBJECT
-
-    std::string name;
-    std::string id;
-    std::string version;
-
 public:
     CDND(QVBoxLayout* parent = nullptr, std::string name = "");
     void dragEnterEvent(QDragEnterEvent* e);
@@ -36,9 +30,5 @@ public:
 
 signals:
     void launch();
-
-protected:
-    void modInfoSave(const std::filesystem::path& directory);
-
 };
 #endif // DND_H
